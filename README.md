e501cb3161d22cf99ab59afcaffd1707c53977f0
nicoptere <nicoptere@gmail.com>
2022-09-05 23:19:49 +0200

    added upscale

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 #### foreword
 
 this is a toy project to run the [Stable Diffusion](https://github.com/CompVis/stable-diffusion) model locally.
-by _toy project_ I mean the UI is not user friendly, the code is terribly messy and it will stay that way.
-if you're after something solid, **I'd suggest you use [WebUI](https://github.com/hlky/stable-diffusion-webui) instead**.
+if you're after something more solid, I'd suggest you use [WebUI](https://github.com/hlky/stable-diffusion-webui) instead.
 
-this is essentially a bridge between NodeJS and the [huggingface diffusers](https://github.com/huggingface/diffusers).
+as of now, this is essentially a bridge between NodeJS and the [huggingface diffusers](https://github.com/huggingface/diffusers).
 you'll find all the background information and interactive exampless on the [huggingface diffusers colab](https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/stable_diffusion.ipynb#scrollTo=gd-vX3cavOCt).
 
 #### pre-requisite :
@@ -20,32 +19,43 @@ you'll find all the background information and interactive exampless on the [hug
 - Python 3.X (I use 3.9.5 on Windows) + PIP
 - NodeJS and NPM
 
-in other words:
+in other words, if you managed to run the [huggingface diffusers](https://github.com/huggingface/diffusers) locally, from a CLI, you should be good to go. if not, I can't really help as the setup varies immensely between machines, OS, CUDA and Python version.
 
-if you manage to run the [huggingface diffusers](https://github.com/huggingface/diffusers) locally, from a CLI, you should be good to go.
+#### upscaling:
 
-if not, I can't be of any help as the setup varies immensely between machines.
+the upscaling option will require you to install RealESRGAN and CFPGAN (for face enhancement)
+
+```
+pip install realesrgan==0.2.5.0
+pip install gfpgan==0.2.4
+```
+
+feel free to install more models from the [REALESRGAN model zoo](https://upscale.wiki/wiki/Model_Database#Paper.2FOfficial_Models), make sure to save them into the `python/models/` folder.
+
+**warning:**
+
+> I removed the **NSFW filter** on the _img2img.py_ because it returns empty images if it considers the content unsafe. this means that if you provide🔞prompts, the model **will deliver🔞contents**.
+> consider yourself warned.
 
 #### run!
 
 install the npm dependencies with `npm install`
 
-then run the server with `npm run sd`
+the JS app is located in src/, you can (must) build it with `rollup --config rollup.config.js`
 
-then open a web browser and got to `http://localhost:8080/`
+run the server with `npm start` or `npm run sd `or `node server.js`
 
-#### usage
+open a web browser and got to `http://localhost:8080/ `
 
-**warning:**
+to test on another device (mobile, tablet), set your LAN IP in the server.js file.
 
-- I removed the **NSFW filter** on the _img2img.py_
-  this means that if you provide🔞prompts, the model **will deliver**.
-  consider yourself warned.
-- after a while the thing blows up and may crash your machine (gently).
+## overview
 
-The concept is to generate images using using text written in natural language: the **prompts**. you can write them inside the textareas or press the **randomize** button below to use the built in prompts list from [Prompt Parrot! 🦜](https://colab.research.google.com/drive/1GtyVgVCwnDfRvfsHbeU0AlG-SgQn1p8e?usp=sharing#scrollTo=gbfhp-raJgPH) to quickly check various styles and configs.
+the concept is to generate images using using text written in natural language: the **prompts**.
 
-the thing works on a fullscreen 2D canvas and the draggable red rectangle is the area that will be sent and processed by Python.
+you can write them inside the textareas or press the **randomize** button below to use the built in prompts list from [Prompt Parrot! 🦜](https://colab.research.google.com/drive/1GtyVgVCwnDfRvfsHbeU0AlG-SgQn1p8e?usp=sharing#scrollTo=gbfhp-raJgPH) to quickly check various styles and configs.
+
+the app works with a single 2D canvas and the draggable red rectangle is the "zone" that will be processed by Python. this allows for interesting and intuitive edition capabilities: start with an inference, then inptaint a larger area, then use image to image on portions of the resulting composition, rinse and repeat.
 
 #### inference
 
@@ -65,12 +75,12 @@ the **inpainting** tab works a bit like the image to image. the difference is th
 the mask is sent alongt the source image and the model will only _drive_ the painted area towards the prompt.
 there are 3 extra params on this tab: **size**, the brush size, **softness**, the _strength_ of the brush gradient and **alpha**, the opacity of the stroke. this is a rather incredible feature when you get used to it (addictive too).
 
-#### size
+#### zone size
 
-it is recommended to keep 512 in width or height, smaller sizes (say 256*256) won't give good results (they'll be oversaturated). I limited the rect to 512*512 as it may become impossible for Node to receive Blobs bigger than that...
-
-#### app
-
-the JS app is located in src/, you can re-build it with `rollup --config rollup.config.js`
+it is recommended to keep 512 in width or height, smaller sizes (say 256*256) won't give good results (they'll be oversaturated). I limited the rect to 512*512 as it may become impossible for Node to receive Blobs bigger than that.
 
 feedbacks welcome :)
+
+## UI
+
+all the functionalities are packed in a single GUI, the parameters vary depending on what you want to do.
---
+++ python/upscale.py
@@ -0,0 +1,190 @@
+import argparse
+import cv2
+import glob
+import requests
+import os
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from realesrgan import RealESRGANer
+from realesrgan.archs.srvgg_arch import SRVGGNetCompact
+
+# creates a folder to store the images
+ROOT_FOLDER = "upscale/"
+os.makedirs(ROOT_FOLDER, exist_ok=True)
+
+DEBUG = True
+
+
+def sanitize(text):
+    # removes invalid characters from the prompt
+    stripped_text = ''
+    for c in text:
+        stripped_text += c if len(c.encode(encoding='utf_8')) == 1 else ''
+    return stripped_text
+
+
+@pytalk_method('upscale')
+def upscale(input, face_enhance=True, model_name='RealESRGAN_x4plus', outscale=4):
+
+    output = ROOT_FOLDER
+    suffix = ""
+    if face_enhance == True:
+        suffix = '-gfp'
+    tile = 0
+    tile_pad = 10
+    pre_pad = 0
+    fp32 = True
+    alpha_upsampler = 'realesrgan'
+    ext = 'jpg'
+
+    """Inference demo for Real-ESRGAN.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str,
+                        default='inputs', help='Input image or folder')
+    parser.add_argument(
+        '-n',
+        '--model_name',
+        type=str,
+        default='RealESRGAN_x4plus',
+        help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
+              'realesr-animevideov3'))
+    parser.add_argument('-o', '--output', type=str,
+                        default='results', help='Output folder')
+    parser.add_argument('-s', '--outscale', type=float, default=4,
+                        help='The final upsampling scale of the image')
+    parser.add_argument('--suffix', type=str, default='out',
+                        help='Suffix of the restored image')
+    parser.add_argument('-t', '--tile', type=int, default=0,
+                        help='Tile size, 0 for no tile during testing')
+    parser.add_argument('--tile_pad', type=int,
+                        default=10, help='Tile padding')
+    parser.add_argument('--pre_pad', type=int, default=0,
+                        help='Pre padding size at each border')
+    parser.add_argument('--face_enhance', action='store_true',
+                        help='Use GFPGAN to enhance face')
+    parser.add_argument(
+        '--fp32', action='store_true', help='Use fp32 precision during inference. Default: fp16 (half precision).')
+    parser.add_argument(
+        '--alpha_upsampler',
+        type=str,
+        default='realesrgan',
+        help='The upsampler for the alpha channels. Options: realesrgan | bicubic')
+    parser.add_argument(
+        '--ext',
+        type=str,
+        default='auto',
+        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
+    parser.add_argument(
+        '-g', '--gpu-id', type=int, default=None, help='gpu device to use (default=None) can be 0,1,2 for multi-gpu')
+
+    args = parser.parse_args()
+    """
+
+    # determine models according to model names
+    model_name = model_name.split('.')[0]
+    if model_name in ['RealESRGAN_x4plus', 'RealESRNet_x4plus']:  # x4 RRDBNet model
+        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
+                        num_block=23, num_grow_ch=32, scale=4)
+        netscale = 4
+    # x4 RRDBNet model with 6 blocks
+    elif model_name in ['RealESRGAN_x4plus_anime_6B']:
+        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
+                        num_block=6, num_grow_ch=32, scale=4)
+        netscale = 4
+    elif model_name in ['RealESRGAN_x2plus']:  # x2 RRDBNet model
+        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
+                        num_block=23, num_grow_ch=32, scale=2)
+        netscale = 2
+    # x4 VGG-style model (XS size)
+    elif model_name in ['realesr-animevideov3']:
+        model = SRVGGNetCompact(
+            num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+        netscale = 4
+
+    # determine model paths
+
+    model_path = os.path.join(
+        'python/models', model_name + '.pth')
+    if not os.path.isfile(model_path):
+
+        # model_path = os.path.join(
+        #     'python/realesrgan/weights', model_name + '.pth')
+
+        # download the X4 model
+        url = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth'
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            with open(model_path, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+        else:
+            print("download failed:", str(r.status_code), url)
+
+    if not os.path.isfile(model_path):
+        raise ValueError(f'Model {model_name} does not exist.')
+
+    # restorer
+    upsampler = RealESRGANer(
+        scale=netscale,
+        model_path=model_path,
+        model=model,
+        tile=tile,
+        tile_pad=tile_pad,
+        pre_pad=pre_pad,
+        half=not fp32)
+
+    if face_enhance:  # Use GFPGAN for face enhancement
+        from gfpgan import GFPGANer
+        face_enhancer = GFPGANer(
+            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
+            upscale=outscale,
+            arch='clean',
+            channel_multiplier=2,
+            bg_upsampler=upsampler)
+    os.makedirs(output, exist_ok=True)
+
+    if os.path.isfile(input):
+        paths = [input]
+    else:
+        paths = sorted(glob.glob(os.path.join(input, '*')))
+
+    for idx, path in enumerate(paths):
+        imgname, extension = os.path.splitext(os.path.basename(path))
+        print('Testing', idx, imgname)
+
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        if len(img.shape) == 3 and img.shape[2] == 4:
+            img_mode = 'RGBA'
+        else:
+            img_mode = None
+
+        image = None
+        try:
+            if face_enhance:
+                _, _, image = face_enhancer.enhance(
+                    img, has_aligned=False, only_center_face=False, paste_back=True)
+            else:
+                image, _ = upsampler.enhance(img, outscale=outscale)
+
+        except RuntimeError as error:
+            print('Error', error)
+            print(
+                'If you encounter CUDA out of memory, try to set --tile with a smaller number.')
+        else:
+            if ext == 'auto':
+                extension = extension[1:]
+            else:
+                extension = ext
+            if img_mode == 'RGBA':  # RGBA images should be saved in png format
+                extension = 'png'
+            save_path = os.path.join(
+                output, '%s%s.%s' % (imgname, suffix, extension))
+            print()
+            cv2.imwrite(save_path, image)
+
+
+if __name__ == "__main__":
+    print('hey')
+    url = "inference/inf-beautiful-cyberpunk-portrait-by-Liam-Wong-and-Ross-Tran,-artstation,-dark,-contrasting,-noir_40_7.34_3260_384_512.png"
+    url = "inference/inf-Lilith,-ada-Lovelace-portrait,-Deadpool,-gothic,-artstation,-oil-on-canvas_54_12.77_1_512_512.png"
+    upscale(url, False)
+    upscale(url, True)
